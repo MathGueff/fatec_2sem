a243c098a61274a0f46d9d34b756d18afbe0eef3
MatheusGueff <gueffmatheus@gmail.com>
2024-08-29 20:27:24 -0300

    Adicionado o projeto de Angular

--- DesenvolvimentoWeb/aula01-Typescript/ex01.js
+++ DesenvolvimentoWeb/aula01-Typescript/ex01.js
@@ -2,13 +2,13 @@
 //Exercicio
 //Criar um programa que receba um número e mostre seus dois antecessores e sucessores
 Object.defineProperty(exports, "__esModule", { value: true });
-const promptSync = require("prompt-sync");
-const prompt = promptSync();
+var promptSync = require("prompt-sync");
+var prompt = promptSync();
 var numero = Number(prompt("Diga um número: "));
 var antecessor = numero - 1;
 var sucessor = numero + 1;
 var antecessor2 = numero - 2;
 var sucessor2 = numero + 2;
-console.log(`Número: ${numero}`);
-console.log(`Antecessor: ${antecessor}, segundo antecessor ${antecessor2}`);
-console.log(`Sucessor: ${sucessor}, segundo sucessor ${sucessor2}`);
+console.log("N\u00FAmero: ".concat(numero));
+console.log("Antecessor: ".concat(antecessor, ", segundo antecessor ").concat(antecessor2));
+console.log("Sucessor: ".concat(sucessor, ", segundo sucessor ").concat(sucessor2));
