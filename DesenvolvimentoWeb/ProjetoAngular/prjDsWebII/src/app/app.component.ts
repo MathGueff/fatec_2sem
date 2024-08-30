cde8797ceb09fb40f819e52c16c29adba104e7bc
MatheusGueff <gueffmatheus@gmail.com>
2024-08-29 22:07:06 -0300

    Adicionado os componentes header, footer e main do projeto Angular

--- DesenvolvimentoWeb/ProjetoAngular/prjDsWebII/src/app/app.component.ts
+++ DesenvolvimentoWeb/ProjetoAngular/prjDsWebII/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainComponent } from "./main/main.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'prjDsWebII';
+  title = 'Projeto Desenvolvimento Web II';
+  message = 'Hello World';
 }
