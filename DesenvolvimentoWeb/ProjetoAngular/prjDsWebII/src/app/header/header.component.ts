ea9ce25b7b94f109ce264e95c468f7e90b55c6b4
MatheusGueff <gueffmatheus@gmail.com>
2024-09-05 22:07:22 -0300

    Adicionado o arquivo com Angular

--- DesenvolvimentoWeb/ProjetoAngular/prjDsWebII/src/app/header/header.component.ts
+++ DesenvolvimentoWeb/ProjetoAngular/prjDsWebII/src/app/header/header.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  titulosite = "EXTENSÕES CACHORROS";
+  menuusuario = false
 }
